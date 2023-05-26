0d59ce29638ee7503fd8e92d88823e1d00c8dd8d
lucasgp7 <lucasgpaula7@gmail.com>
2023-05-26 09:17:30 -0300

    Adicionado config.properties no lugar do json, biblioteca de log, validação do ultimo importado e uso de 2 bancos MYSQL e Postgres

--- connect.py
+++ connect.py
@@ -1,29 +1,53 @@
+import configparser
 import psycopg2
-import json
+import mysql.connector
+import logging
 
 def connect_to_database():
     # Lê o arquivo de configuração
-    with open('config.json') as config_file:
-        config = json.load(config_file)
+    config = configparser.ConfigParser()
+    config.read('config.properties')
+
+    database_type = config.get('database', 'database')
 
     try:
         # Conectar ao banco de dados PostgreSQL
-        conn = psycopg2.connect(
-            host=config['host'],
-            port=config['port'],
-            database=config['database'],
-            user=config['user'],
-            password=config['password']
-        )
+        if database_type == 'postgresql':
+            conn = psycopg2.connect(
+                host=config.get('postgresql', 'postgresql.host'),
+                port=config.get('postgresql', 'postgresql.port'),
+                database=config.get('postgresql', 'postgresql.database'),
+                user=config.get('postgresql', 'postgresql.user'),
+                password=config.get('postgresql', 'postgresql.password')
+            )
+            cursor = conn.cursor()
 
-        cursor = conn.cursor()
+        # Conectar ao banco de dados MySQL
+        elif database_type == 'mysql':
+            conn = mysql.connector.connect(
+                host=config.get('mysql', 'mysql.host'),
+                port=config.get('mysql', 'mysql.port'),
+                database=config.get('mysql', 'mysql.database'),
+                user=config.get('mysql', 'mysql.user'),
+                password=config.get('mysql', 'mysql.password')
+            )
+            cursor = conn.cursor()
+
+        else:
+            logging.error("Tipo de banco de dados inválido no arquivo de configuração.")
+            return None, None, False
+
+        # Configurar o logger para exibir mensagens no console
+        logging.basicConfig(level=logging.INFO)
+
+        # Registrar a confirmação da conexão no log e exibir no console
+        logging.info("Conexão com o banco de dados " + database_type + " estabelecida com sucesso!")
+        print("Conexão com o banco de dados", database_type, "estabelecida com sucesso!")
 
         # Retornar conexão e cursor
         return conn, cursor, True
 
-    except (Exception, psycopg2.Error) as error:
+    except (Exception, psycopg2.Error, mysql.connector.Error) as error:
+        logging.error("Erro ao conectar ao banco de dados: " + str(error))
         print("Erro ao conectar ao banco de dados:", error)
         return None, None, False
-    
-# Utilização da função
-conn, cursor, connected = connect_to_database()
--- main_bytea.py
+++ main_bytea.py
@@ -1,6 +1,7 @@
 import os
 import datetime
 import aspose.words as aw
+import logging
 from bs4 import BeautifulSoup
 from connect import *
 
@@ -8,35 +9,36 @@ def convert_rtf_to_html(rtf_file, html_file):
     doc = aw.Document(rtf_file)
     doc.save(html_file)
 
-# Utilização da função
+# Utilização da função de conexão do banco
 conn, cursor, connected = connect_to_database()
-
-if connected:
-    print("Conexão com o banco de dados estabelecida com sucesso!")
-    # Restante do código para realizar operações no banco de dados
-else:
-    print("Falha ao conectar ao banco de dados.")
-    # Lidar com a falha na conexão
-
 cursor = conn.cursor()
 
+# Criar um objeto ConfigParser, ler o config.properties e pega o valor do schema
+config = configparser.ConfigParser()
+config.read('config.properties')
+schema = config.get('schema', 'schema')
+
 # Definir a tabela de entrada e a tabela de saída
-input_table = "legacy.laudo_legado"
-output_table = "legacy.laudo_import_html"
+input_table = schema + ".laudo_legado"
+output_table = schema + ".laudo_import_html"
+
+# Configurar o logger
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s')
 
 # Criar a tabela de saída se ela não existir
 create_table_query = f"""
-    CREATE TABLE IF NOT EXISTS legacy.laudo_import_html (
+    CREATE TABLE IF NOT EXISTS {output_table}(
         id SERIAL PRIMARY KEY,
         id_laudo VARCHAR,
         texto TEXT,
-        erro TEXT,
-        dt_import_laudo TIMESTAMP
+        error_message TEXT,
+        dt_import_laudo TIMESTAMP,
+        convertido BOOLEAN DEFAULT FALSE 
     )
 """
 cursor.execute(create_table_query)
 conn.commit()
-
 
 # Adicionar um índice na coluna "id_laudo" da tabela de saída
 add_index_query = f"CREATE INDEX IF NOT EXISTS idx_id_laudo ON {output_table} (id_laudo)"
@@ -44,44 +46,48 @@ cursor.execute(add_index_query)
 conn.commit()
 
 # Ler os dados da tabela de entrada
-select_query = f"SELECT idlaudo, laudo FROM {input_table}"
+select_query = f"SELECT idlaudo, laudo FROM {input_table} WHERE idlaudo IS NOT NULL AND idlaudo != ''"
 cursor.execute(select_query)
 rows = cursor.fetchall()
 
+# Obter o último id_laudo lido a partir do log (se existir)
+ultimo_id_laudo_lido = None
+# Aqui você pode adicionar a lógica para obter o último id_laudo lido do seu serviço
+
+# Iterar pelas linhas da tabela de entrada
+for idlaudo, laudo in rows:
+    if idlaudo == ultimo_id_laudo_lido:
+        # Encontrado o último id_laudo lido, continuar para o próximo
+        continue
+
 # Obter a data e hora atual
 current_datetime = datetime.datetime.now()
+
+errors = []  # Lista para armazenar os erros encontrados
 
 # Converter cada arquivo RTF para HTML e salvar na tabela de saída
 for row in rows:
     file_id, rtf_text = row
     rtf_file = f"input_{file_id}.rtf"
-    html_file = f"output_{file_id}.html"
-
-    # Verificar se o ID é nulo/vazio
-    if file_id is None or file_id == '':
-        print(f"Arquivo ID: {file_id} Laudo já convertido ou ID nulo/vazio, passando para o próximo.")
-        # Inserir o erro na coluna "erro" da tabela de saída
-        insert_error_query = f"INSERT INTO {output_table} (id_laudo, erro, dt_import_laudo) VALUES (%s, %s, %s)"
-        cursor.execute(insert_error_query, (None, "ID é nulo ou vazio", current_datetime))
-        conn.commit()
-        print(f"ID é nulo ou vazio")
-        continue
 
     # Verificar se o ID já foi convertido
-    check_query = f"SELECT id_laudo FROM laudo_import_html WHERE id_laudo = %s"
-    cursor.execute(check_query, (file_id,))
+    check_query = "SELECT id_laudo FROM " + output_table + " WHERE id_laudo = %(file_id)s"
+    cursor.execute(check_query, {"file_id": file_id})
     result = cursor.fetchone()
+
     if result:
-        # Inserir na coluna erro se o laudo já foi convertido
-        insert_error_query = f"INSERT INTO laudo_import_html (id_laudo, erro, dt_import_laudo) VALUES (%s, %s, %s)"
-        cursor.execute(insert_error_query, (file_id, f"Arquivo ID: {file_id} Laudo já convertido, passando para o próximo.", current_datetime))
+        error_message = f"Arquivo ID: {file_id} Laudo já convertido"
+        update_query = f"UPDATE {output_table} SET error_message = %(error_message)s WHERE id_laudo = %(file_id)s"
+        cursor.execute(update_query, {"error_message": error_message, "file_id": file_id})
         conn.commit()
-        print(f"Arquivo ID: {file_id} Laudo já convertido, passando para o próximo.")
+        errors.append((file_id, error_message, current_datetime))
         continue
 
-     # Salvar o conteúdo do RTF em um arquivo temporário
+    # Salvar o conteúdo do RTF em um arquivo temporário
     with open(rtf_file, "wb") as file:
         file.write(rtf_text)
+    # Nome do arquivo HTML
+    html_file = f"output_{file_id}.html"
 
     try:
         # Converter o arquivo RTF para HTML
@@ -95,7 +101,7 @@ for row in rows:
         soup = BeautifulSoup(html_text, "html.parser")
         for img_tag in soup.find_all("img"):
             img_tag.extract()
-        
+
         for table in soup.find_all('table'):
             table.extract()
 
@@ -106,28 +112,32 @@ for row in rows:
             if tag.string and tag.string.strip() == '':
                 tag.extract()
 
-        # Realizar o replace no texto
+        # Realiza o replace no html
         html_text = str(soup)
-        html_text = html_text.replace("Evaluation Only. Created with Aspose.Words. Copyright 2003-2023 Aspose Pty Ltd.", "")
-        html_text = html_text.replace("Created with an evaluation copy of Aspose.Words. To discover the full versions of our APIs please visit: https://products.aspose.com/words/", "")
+        html_text = html_text.replace("Evaluation Only. Created with Aspose.Words. Copyright 2003-2023 Aspose Pty Ltd.","")
+        html_text = html_text.replace("Created with an evaluation copy of Aspose.Words. To discover the full versions of our APIs please visit: https://products.aspose.com/words/","")
         html_text = html_text.replace("<title></title></head><body><div><div><p><span></span></p><p></p></div><p><span></span></p><p>", "")
         html_text = html_text.replace('<meta content="Aspose.Words for Python via .NET 23.4.0" name="generator"/></p><p><br/></p><p>', '')
-        html_text = html_text.replace("Nome:","")
-        html_text = html_text.replace("Registro:","")
-        html_text = html_text.replace("Convênio:","")
-        html_text = html_text.replace("Idade:","")
-        html_text = html_text.replace("Médico:","")
-        html_text = html_text.replace("Data:","")
-        
+        html_text = html_text.replace("Nome:", "")
+        html_text = html_text.replace("Registro:", "")
+        html_text = html_text.replace("Convênio:", "")
+        html_text = html_text.replace("Idade:", "")
+        html_text = html_text.replace("Médico:", "")
+        html_text = html_text.replace("Data:", "")
+
         # Atribuir o conteúdo HTML diretamente à variável body_content
         body_content = html_text
 
         # Inserir o conteúdo HTML, o ID e a data/hora de importação na tabela de saída
-        insert_query = f"INSERT INTO {output_table} (id_laudo, texto, dt_import_laudo) VALUES (%s, %s, %s)"
-        cursor.execute(insert_query, (file_id, body_content, current_datetime))
+        insert_query = f"INSERT INTO {output_table} (id_laudo, texto, dt_import_laudo, convertido) VALUES (%s, %s, %s, %s)"
+        cursor.execute(insert_query, (file_id, body_content, current_datetime, True))
         conn.commit()
 
-        print(f"Arquivo ID: {file_id} convertido com sucesso.")
+        # Registrar as informações relevantes no log
+        logging.info(f"ID Laudo: {file_id} importado com sucesso")
+
+        # Atualizar o último id_laudo lido no seu serviço
+        ultimo_id_laudo_lido = file_id
 
         # Remover os arquivos temporários
         os.remove(rtf_file)
@@ -137,12 +147,14 @@ for row in rows:
         for filename in os.listdir('.'):
             if filename.endswith(('.png', '.jpeg', '.jpg')):
                 os.remove(filename)
-                
+
+    # Registrar eventuais erros no log
     except Exception as e:
-        print(f"Erro ao converter o arquivo ID: {file_id} - {str(e)}")
+        logging.error(f"Erro ao converter o arquivo ID: {file_id} - {str(e)}")
 
 # Fechar a conexão
 cursor.close()
 conn.close()
+# Finalização da conversão no log
+logging.info("Conversão concluída. Os arquivos RTF foram convertidos para HTML e salvos na tabela do PostgreSQL.")
 
-print("Conversão concluída. Os arquivos RTF foram convertidos para HTML e salvos na tabela do PostgreSQL.")
